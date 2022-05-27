99920e27fdb6e5136d09000535ac11db0174e423
SmallCorinE <qqqqmmss11@gmail.com>
2022-05-28 00:08:23 +0900

    done.

--- button.js
+++ button.js
@@ -1,20 +1,23 @@
 var Link = {
 	setLinkColor:function (color){
-		var alist = document.querySelectorAll('a');
-		var i = 0;
-		while(i<alist.length){
-			console.log(alist[i]);
-			alist[i].style.color=color;
-			i=i+1;
-		}
+		// var alist = document.querySelectorAll('a');
+		// var i = 0;
+		// while(i<alist.length){
+		// 	console.log(alist[i]);
+		// 	alist[i].style.color=color;
+		// 	i=i+1;
+		// }
+		$('a').css('color', color);
 	}
 }
 var Body = {
 	setLetterColor:function (color){
-		document.querySelector('body').style.color=color;
+		// document.querySelector('body').style.color=color;
+		$('body').css('color', color);
 	},
 	setBackGColor:function (color){
-		document.querySelector('body').style.backgroundColor=color;
+		// document.querySelector('body').style.backgroundColor=color;
+		$('body').css('backgroundColor', color);
 	}
 }
 
