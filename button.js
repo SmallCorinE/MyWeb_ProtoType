a84fd31da38477249117618c1c09742890c22a96
SmallCorinE <qqqqmmss11@gmail.com>
2022-05-27 23:21:58 +0900

    all done?

---
+++ button.js
@@ -0,0 +1,35 @@
+var Link = {
+	setLinkColor:function (color){
+		var alist = document.querySelectorAll('a');
+		var i = 0;
+		while(i<alist.length){
+			console.log(alist[i]);
+			alist[i].style.color=color;
+			i=i+1;
+		}
+	}
+}
+var Body = {
+	setLetterColor:function (color){
+		document.querySelector('body').style.color=color;
+	},
+	setBackGColor:function (color){
+		document.querySelector('body').style.backgroundColor=color;
+	}
+}
+
+	function contrastHandler(self){
+		var target = document.querySelector('body');
+		if(self.value === 'Make it bright'){
+			Body.setBackGColor('white')
+			Body.setLetterColor('black')
+			self.value = 'Make it dark';
+			Link.setLinkColor('red')
+
+		} else {
+			Body.setBackGColor('black')
+			Body.setLetterColor('white')
+			self.value = 'Make it bright';
+			Link.setLinkColor('pink');
+		}
+	}
